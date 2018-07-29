a93dcdf1789c1ff0295063e2c65441c8c08cfb20
loanpasionek <l.pasionek@gmail.com>
2018-07-29 14:19:55 +0200

    Correctif sur le JS de la nav, changement de couleur bien applique selon position dans la page

--- public/js/script.js
+++ public/js/script.js
@@ -18,6 +18,25 @@ $('a[href^="#"]').on('click', function(event) {
 });
 
 
+/* Meme fonction sur écrans < 800 */
+
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    var nav = $('nav').height();
+    if(screen.width <= 800){
+    if(target.length) {
+        event.preventDefault();
+
+
+        var scroll = target.offset().top;
+        
+        $('html, body').stop().animate({
+            scrollTop: scroll
+        }, 900);
+    }
+}
+});
+
 /* ------------  Bouton flottant en small screen ------------  */
 
 /* $(document).ready(function(){
@@ -26,115 +45,112 @@ $('a[href^="#"]').on('click', function(event) {
 
 
 
-/* ------------  Parallax ------------  */
-
-
 
 /* ------------  Nav changeant sur scrolling ------------  */
 
     $(function(){
-
+        var containerNav = $('#containerNav');
         var nav = $('nav');
         var navHeight = nav.height();
         var windowHeight = $(window).height();
         var presentation = $('#presentation');
-        var coloredChar = $('.coloredChars1');
+        var coloredChar1 = $('.coloredChars1');  /* Toutes les lettres orange sauf le "C" de Code */
         var coloredChar2 = $('.coloredChars2');
         $(window).scroll(function()
     {
         var scroll = $(window).scrollTop();
 
         /* Application de la fonction sur écrans larges > 800 */
-    if (screen.width > 800){
-        if (scroll > windowHeight-navHeight){
-            nav.removeClass('nocolor').addClass('navChange');
-        }
-        else{
-            if(scroll <= windowHeight-navHeight){
-            nav.removeClass('navChange').addClass('nocolor');
+        if (screen.width > 800){
+            if (scroll > windowHeight/2){
+                nav.removeClass('nocolor').addClass('navChange');
             }
-            
-        }
-
-        if (scroll > windowHeight-navHeight*2){
-            presentation.addClass('hidden');
-            coloredChar.removeClass('color1').addClass('hidden');
-        }
-        else{
-            if(scroll <= windowHeight-navHeight*2){
-            presentation.removeClass('hidden');
-            coloredChar.removeClass('hidden').addClass('color1');
+            else{
+                if(scroll <= windowHeight/2){
+                    nav.removeClass('navChange').addClass('nocolor');
+                }
+                
             }
-            
-        }
 
-        if(scroll > windowHeight-navHeight*3){
+            if (scroll > windowHeight-navHeight*2){
+                presentation.addClass('hidden');
+                coloredChar1.removeClass('color1').addClass('hidden');
+            }
+            else{
+                if(scroll <= windowHeight-navHeight*2){
+                presentation.removeClass('hidden');
+                coloredChar1.removeClass('hidden').addClass('color1');
+                }
+                
+            }
 
-            coloredChar2.removeClass('color1').addClass('hidden');
-        }
+            if(scroll > windowHeight-navHeight*3){
 
-        else{
-            if(scroll <= windowHeight-navHeight*3){
-                coloredChar2.removeClass('hidden').addClass('color1');
+                coloredChar2.removeClass('color1').addClass('hidden');
+            }
+
+            else{
+                if(scroll <= windowHeight-navHeight*3){
+                    coloredChar2.removeClass('hidden').addClass('color1');
+                }
+            }
+
+            if (scroll > windowHeight*2){
+                nav.removeClass('nocolor').addClass('bgcolor3');
+            }
+            else{
+                if(scroll < windowHeight){
+                nav.removeClass('bgcolor3').addClass('nocolor');
+                }
+                
             }
         }
-
-        if (scroll > windowHeight*2){
-            nav.removeClass('nocolor').addClass('bgcolor3');
-        }
-        else{
-            if(scroll < windowHeight){
-            nav.removeClass('bgcolor3').addClass('nocolor');
-            }
-            
-        }
-    }
         
-    /* Application de la fonction sur écrans < 800 */
-    if (screen.width <= 800){
-        if (scroll > windowHeight-navHeight){
-            nav.removeClass('nocolor').addClass('navChange');
-        }
-        else{
-            if(scroll <= windowHeight-navHeight){
-            nav.removeClass('navChange').addClass('nocolor');
+        /* Application de la fonction sur écrans < 800 */
+        if (screen.width <= 800){
+            if (scroll > windowHeight-navHeight){
+                nav.removeClass('nocolor').addClass('navChange');
             }
-            
-        }
-
-        if (scroll > windowHeight-navHeight*2){
-            presentation.addClass('hidden');
-            coloredChar.removeClass('color1').addClass('hidden');
-        }
-        else{
-            if(scroll <= windowHeight-navHeight*2){
-            presentation.removeClass('hidden');
-            coloredChar.removeClass('hidden').addClass('color1');
+            else{
+                if(scroll <= windowHeight-navHeight){
+                nav.removeClass('navChange').addClass('nocolor');
+                }
+                
             }
-            
-        }
 
-        if(scroll > windowHeight-navHeight*2+20){
+            if (scroll > windowHeight-navHeight*2){
+                presentation.addClass('hidden');
+                coloredChar1.removeClass('color1').addClass('hidden');
+            }
+            else{
+                if(scroll <= windowHeight-navHeight*2){
+                presentation.removeClass('hidden');
+                coloredChar1.removeClass('hidden').addClass('color1');
+                }
+                
+            }
 
-            coloredChar2.removeClass('color1').addClass('hidden');
-        }
+            if(scroll > windowHeight-navHeight*2+20){
 
-        else{
-            if(scroll <= windowHeight-navHeight*2+20){
-                coloredChar2.removeClass('hidden').addClass('color1');
+                coloredChar2.removeClass('color1').addClass('hidden');
+            }
+
+            else{
+                if(scroll <= windowHeight-navHeight*2+20){
+                    coloredChar2.removeClass('hidden').addClass('color1');
+                }
+            }
+
+            if (scroll > windowHeight*2){
+                containerNav.removeClass('nocolor').addClass('bgcolor3');
+            }
+            else{
+                if(scroll < windowHeight){
+                containerNav.removeClass('bgcolor3').addClass('nocolor');
+                }
+                
             }
         }
-
-        if (scroll > windowHeight*2){
-            nav.removeClass('nocolor').addClass('bgcolor3');
-        }
-        else{
-            if(scroll < windowHeight){
-            nav.removeClass('bgcolor3').addClass('nocolor');
-            }
-            
-        }
-    }
 
     })
 
